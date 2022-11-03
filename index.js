5d908bd56cdb0b3a2291944dbc2c22b47d67d97b
MrEleffant <90548369+MrEleffant@users.noreply.github.com>
2022-11-03 13:15:51 +0100

    adding temp and changing output format

--- index.js
+++ index.js
@@ -5,40 +5,48 @@ const motsCles = require('./data/mots.json')
 const output = require('./data/output.json')
 let page; let browser
 
-prog()
-
-async function prog () {
+(async () => {
   browser = await puppeteer.launch({
     headless: false
   })
   page = await browser.newPage()
 
   for (const mot of motsCles) {
-    console.log(mot)
-    await page.goto(`https://www.google.fr/search?q=${mot}`, { waitUntil: 'networkidle2' })
-    const searchResults = await page.$$eval('.LC20lb', (els) =>
-      els.map((e) => ({ title: e.innerText, link: e.parentNode.href }))
-    )
-    searchResults.forEach((research) => {
-      // research.domain = getDomain(research.link)
-      const domain = getDomain(research.link)
-      if (!output[domain]) {
-        output[domain] = {
-          motsClesDetectes: [],
-          liensExacts: []
-        }
-      }
-      if (!output[domain].motsClesDetectes.includes(mot)) output[domain].motsClesDetectes.push(mot)
-      output[domain].liensExacts.push(research.link)
-      writeJsonFileUTF8('./data/output.json', output)
-    })
-    console.log(searchResults)
+    await traitement(mot)
   }
+})()
+
+async function traitement (mot) {
+  await wait(5000) // attente de 5 secondes entre les requêtes
+  console.log(mot)
+  await page.goto(`https://www.google.fr/search?q=${mot}`, { waitUntil: 'networkidle2' })
+  const searchResults = await page.$$eval('.LC20lb', (els) =>
+    els.map((e) => ({ title: e.innerText, link: e.parentNode.href }))
+  )
+  searchResults.forEach((research) => {
+    const domain = getDomain(research.link)
+    if (!output[domain]) {
+      output[domain] = []
+    }
+    output[domain].push(
+      {
+        motsCles: mot,
+        liens: research.link
+      })
+    writeJsonFileUTF8('./data/output.json', output)
+  })
+  console.log(searchResults)
 }
 
 function getDomain (url) {
   const arr = url.split('/')
   return arr[0] + '//' + arr[2]
+}
+
+async function wait (ms) {
+  return new Promise(resolve => {
+    setTimeout(resolve, ms)
+  })
 }
 
 function writeJsonFileUTF8 (path, variable) {
