f3b66c7c29c4ae5d99fcda77aedf289e6bb369e6
MrEleffant <90548369+MrEleffant@users.noreply.github.com>
2023-03-31 11:14:05 +0200

    Update code

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
 /**
- *
- .d8888b.  8888888888  .d88888b.
-d88P  Y88b 888        d88P" "Y88b
-Y88b.      888        888     888
- "Y888b.   8888888    888     888
-    "Y88b. 888        888     888
-      "888 888        888     888
-Y88b  d88P 888        Y88b. .d88P
- "Y8888P"  8888888888  "Y88888P"
- *
- *
  * @file index.js
  * @description Main file of the project
- * @author Thibaut Le Louët
  */
-console.log(
-`\n .d8888b.  8888888888  .d88888b.  
+
+console.log(`\n 
+ .d8888b.  8888888888  .d88888b.  
 d88P  Y88b 888        d88P" "Y88b 
 Y88b.      888        888     888 
  "Y888b.   8888888    888     888 
@@ -24,6 +13,7 @@ Y88b.      888        888     888
 Y88b  d88P 888        Y88b. .d88P 
  "Y8888P"  8888888888  "Y88888P"\n\n`)
 console.log('Polytech SEO')
+
 require('dotenv').config()
 const puppeteer = require('puppeteer')
 const inquirer = require('inquirer')
